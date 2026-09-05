f8f710e7c0a02cd1aa5034dd0116ea150cb93474
fnkle <fimble2005@gmail.com>
2026-09-05 14:29:34 +0100

    set up container

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,22 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
-namespace CalendarApp {
+namespace CalendarApp
+{
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
-	public partial class App : Application {
-	}
+	public partial class App : Application
+	{
+		public App()
+		{
+			AppHost = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+			{
+			}).Build();
+		}
 
+		public static IHost AppHost { get; private set; }
+	}
 }
